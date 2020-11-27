f1fc46f46e7c38425f4b15fb2fa62bfb0ac3685c
chrisoney <christopher.oney@yahoo.com>
2020-11-26 21:11:37 -0800

    Add seed data to users

---
+++ db/seeders/20201127050045-addUsers.js
@@ -0,0 +1,46 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: async(queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  const password = await bcrypt.hash('Hunter12!', 10);
+  console.log()
+  return queryInterface.bulkInsert('Users', [
+    {
+      username: 'chris',
+      email: 'chris@chris.com',
+      hashedPassword: password,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'worry',
+      email: 'worry@worry.com',
+      hashedPassword: password,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
