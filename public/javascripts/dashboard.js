9ece91108e750672eda92a052065f35bd7ee6d9e
chrisoney <christopher.oney@yahoo.com>
2020-11-30 21:50:40 -0800

    reorganize dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -4,3 +4,10 @@ document.querySelectorAll(".fa-chevron-circle-left")
     const num = e.target.classList[2].split("-")[1];
     document.querySelector(`.container-${num}`).classList.toggle("open")
   }))
+
+document.querySelectorAll(".reveal")
+  .forEach(ele => ele.addEventListener("click", (e) => {
+    e.preventDefault();
+    const text = e.target.parentElement.lastChild;
+    text.classList.toggle("hidden");
+  }))
