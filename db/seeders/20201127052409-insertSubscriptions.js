39b0a05c99f5ca401ca7b7b9681ae78de5de5448
chrisoney <christopher.oney@yahoo.com>
2020-12-06 21:36:10 -0800

    finish primary seed data

--- db/seeders/20201127052409-insertSubscriptions.js
+++ db/seeders/20201127052409-insertSubscriptions.js
@@ -63,208 +63,208 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'124',
+        book: '0',
+        chapter: '124',
         userId: 1,
         storyId: 6,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'1370',
+        book: '0',
+        chapter: '1370',
         userId: 1,
         storyId: 7,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'436',
+        book: '0',
+        chapter: '436',
         userId: 1,
         storyId: 8,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'340',
+        book: '0',
+        chapter: '340',
         userId: 1,
         storyId: 9,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'64',
+        book: '0',
+        chapter: '64',
         userId: 1,
         storyId: 10,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '3',
+        chapter: '10',
         userId: 1,
         storyId: 11,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '145',
         userId: 1,
         storyId: 12,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '4',
+        chapter: '24',
         userId: 1,
         storyId: 13,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '3',
+        chapter: '9',
         userId: 1,
         storyId: 14,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: 'Day 180 - Blue',
         userId: 1,
         storyId: 15,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '132',
         userId: 1,
         storyId: 16,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '113',
         userId: 1,
         storyId: 17,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '68',
         userId: 1,
         storyId: 18,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '588',
         userId: 1,
         storyId: 19,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '20',
         userId: 1,
         storyId: 20,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '657',
         userId: 1,
         storyId: 21,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '233',
         userId: 1,
         storyId: 22,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '75',
         userId: 1,
         storyId: 23,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '156',
         userId: 1,
         storyId: 24,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '105',
         userId: 1,
         storyId: 25,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '644',
         userId: 1,
         storyId: 26,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '95',
         userId: 1,
         storyId: 27,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '50',
         userId: 1,
         storyId: 28,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '0',
+        chapter: '56',
         userId: 1,
         storyId: 29,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '1',
+        chapter: 'epilogue',
         userId: 1,
         storyId: 30,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book: '2',
+        chapter: '1',
         userId: 1,
         storyId: 31,
         createdAt: new Date(),
@@ -272,7 +272,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'0',
         userId: 1,
         storyId: 32,
         createdAt: new Date(),
@@ -280,7 +280,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'38',
         userId: 1,
         storyId: 33,
         createdAt: new Date(),
@@ -288,15 +288,15 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'Selective Attention (2)',
         userId: 1,
         storyId: 34,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        book:'0',
-        chapter:'',
+        book:'26',
+        chapter:'14',
         userId: 1,
         storyId: 35,
         createdAt: new Date(),
@@ -304,7 +304,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'88',
         userId: 1,
         storyId: 36,
         createdAt: new Date(),
@@ -312,7 +312,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'18',
         userId: 1,
         storyId: 37,
         createdAt: new Date(),
@@ -320,7 +320,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'Loading 0.0',
         userId: 1,
         storyId: 38,
         createdAt: new Date(),
@@ -328,7 +328,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'24',
         userId: 1,
         storyId: 39,
         createdAt: new Date(),
@@ -336,7 +336,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'23',
         userId: 1,
         storyId: 40,
         createdAt: new Date(),
@@ -344,7 +344,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'23',
         userId: 1,
         storyId: 41,
         createdAt: new Date(),
@@ -352,7 +352,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'32',
         userId: 1,
         storyId: 42,
         createdAt: new Date(),
@@ -360,7 +360,7 @@ module.exports = {
       },
       {
         book:'0',
-        chapter:'',
+        chapter:'63',
         userId: 1,
         storyId: 43,
         createdAt: new Date(),
