9244d5771e2e06c7f38bf7720e86fea1ff6d192b
chrisoney <christopher.oney@yahoo.com>
2020-12-12 19:16:36 -0800

    add follow functionality

--- public/javascripts/stories/story.js
+++ public/javascripts/stories/story.js
@@ -2,3 +2,22 @@ document.querySelector('.back-button-container')
   .addEventListener("click", () => {
     window.history.back();
   })
+
+function followEventListeners(){
+  document.querySelectorAll(".follow-button")
+    .forEach(ele => ele.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const followedId = e.target.classList[1].split("-")[1];
+
+      let res = await fetch("/stories/follows/toggle", {
+        credentials: 'same-origin',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ followedId }),
+      });
+      res = await res.json();
+      e.target.innerHTML = e.target.innerHTML === 'Follow' ? 'Unfollow' : 'Follow'
+    }))
+}
