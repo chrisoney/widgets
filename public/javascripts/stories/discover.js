6bf15909db59de8299c7b6157a92a1c0f61f51fc
chrisoney <christopher.oney@yahoo.com>
2020-12-17 22:42:15 -0800

    dry up user discover and follow discover

--- public/javascripts/stories/discover.js
+++ public/javascripts/stories/discover.js
@@ -92,73 +92,73 @@ document.querySelector("#stories").addEventListener("click", async e =>{
   }
 });
 
-function populateStories(following){
-  
+function populateStories(data){
+  const result = [];
+
+  data.forEach(following => {
+    const stories = following.recommendedStories.map((story, idx) => {
+      const rating = populateRating(story.Recommendation.rating);
+      const review = story.Recommendation.review ?
+        `
+        <div class="review-container">
+          <span id="${story.Recommendation.id}" class="reveal">Review</span>
+          <p class="review-text-${story.Recommendation.id} hidden">${story.Recommendation.review}</p>
+        </div>
+        `:``;
+      return `
+      <li class="story-container">
+        <div class="story-header-container">
+          <span class="story-title">${story.title}</span>
+          <div class="story-button-container">
+            <span class="fas fa-chevron-circle-left toggle-${story.Recommendation.id}"></span>
+            <span class="fas fa-arrow-circle-right"></span>
+          </div>
+        </div>
+        <div class="story-details-container container-${story.Recommendation.id}">
+          <div class="story-details-top">
+            <div class="story-details-top-left">
+              <a href=${story.link} class="link">Link to Story</a>
+            </div>
+            <div class="story-details-top-right">
+              <div class="stars">${rating}</div>
+            </div>
+          </div>
+          <div class="story-details-bottom">
+            <div class="description-container">
+              <span id="${idx}" class="reveal">Description</span>
+              <p class="description-text-${idx} hidden">${story.description}</p>
+            </div>
+            ${review}
+          </div>
+        </div>
+      </li>
+      `
+    })
+    if (stories.length === 0){
+      stories.push(`
+        <li class="empty-container">
+          <div class="empty-message">This user hasn't recommended any stories</div>
+        </li>`);
+    }
+    result.push(`
+      <div class="following-container">
+        <div class="following-header">
+          <div class="following-name">${following.username}</div>
+          <div class="follow-button follow-${following.id}">Follow</div>
+        </div>
+        ${stories.join('')}
+      </div>
+    `);
+    
+  })
+  document.querySelector(".tab-content").innerHTML = result.join("");
 }
 
 document.querySelector("#users").addEventListener("click", async e =>{
   try {
     const res = await fetch("/stories/discover-users");
     const data = await res.json()
-    const result = [];
-
-    data.forEach(following => {
-      const stories = following.recommendedStories.map((story, idx) => {
-        const rating = populateRating(story.Recommendation.rating);
-        const review = story.Recommendation.review ?
-          `
-          <div class="review-container">
-            <span id="${story.Recommendation.id}" class="reveal">Review</span>
-            <p class="review-text-${story.Recommendation.id} hidden">${story.Recommendation.review}</p>
-          </div>
-          `:``;
-        return `
-        <li class="story-container">
-          <div class="story-header-container">
-            <span class="story-title">${story.title}</span>
-            <div class="story-button-container">
-              <span class="fas fa-chevron-circle-left toggle-${story.Recommendation.id}"></span>
-              <span class="fas fa-arrow-circle-right"></span>
-            </div>
-          </div>
-          <div class="story-details-container container-${story.Recommendation.id}">
-            <div class="story-details-top">
-              <div class="story-details-top-left">
-                <a href=${story.link} class="link">Link to Story</a>
-              </div>
-              <div class="story-details-top-right">
-                <div class="stars">${rating}</div>
-              </div>
-            </div>
-            <div class="story-details-bottom">
-              <div class="description-container">
-                <span id="${idx}" class="reveal">Description</span>
-                <p class="description-text-${idx} hidden">${story.description}</p>
-              </div>
-              ${review}
-            </div>
-          </div>
-        </li>
-        `
-      })
-      if (stories.length === 0){
-        stories.push(`
-          <li class="empty-container">
-            <div class="empty-message">This user hasn't recommended any stories</div>
-          </li>`);
-      }
-      result.push(`
-        <div class="following-container">
-          <div class="following-header">
-            <div class="following-name">${following.username}</div>
-            <div class="follow-button follow-${following.id}">Follow</div>
-          </div>
-          ${stories.join('')}
-        </div>
-      `);
-      
-    })
-    document.querySelector(".tab-content").innerHTML = result.join("");
+    populateStories(data);
     storyEventListeners();
     followEventListeners();
   } catch (e){
@@ -171,65 +171,7 @@ document.querySelector("#follows").addEventListener("click", async e =>{
   try {
     const res = await fetch("/stories/discover-follows");
     const data = await res.json()
-    const result = [];
-    
-    data.forEach(following => {
-      const stories = following.recommendedStories.map((story, idx) => {
-        const rating = populateRating(story.Recommendation.rating);
-        const review = story.Recommendation.review ?
-          `
-          <div class="review-container">
-            <span id=${story.Recommendation.id} class="reveal">Review</span>
-            <p class="review-text-${story.Recommendation.id} hidden">${story.Recommendation.review}</p>
-          </div>
-          `:``;
-        return `
-        <li class="story-container">
-          <div class="story-header-container">
-            <span class="story-title">${story.title}</span>
-            <div class="story-button-container">
-              <span class="fas fa-chevron-circle-left toggle-${story.Recommendation.id}"></span>
-              <span class="fas fa-arrow-circle-right"></span>
-            </div>
-          </div>
-          <div class="story-details-container container-${story.Recommendation.id}">
-            <div class="story-details-top">
-              <div class="story-details-top-left">
-                <a href=${story.link} class="link">Link to Story</a>
-              </div>
-              <div class="story-details-top-right">
-                <div class="stars">${rating}</div>
-              </div>
-            </div>
-            <div class="story-details-bottom">
-              <div class="description-container">
-                <span id="${story.Recommendation.id}" class="reveal">Description</span>
-                <p class="description-text-${story.Recommendation.id} hidden">${story.description}</p>
-              </div>
-              ${review}
-            </div>
-          </div>
-        </li>
-        `
-      })
-      if (stories.length === 0){
-        stories.push(`
-          <li class="empty-container">
-            <div class="empty-message">This user hasn't recommended any stories</div>
-          </li>`);
-      }
-      result.push(`
-        <div class="following-container">
-          <div class="following-header">
-            <div class="following-name">${following.username}</div>
-            <div class="follow-button follow-${following.id}">Unfollow</div>
-          </div>
-          ${stories.join('')}
-        </div>
-      `);
-      
-    })
-    document.querySelector(".tab-content").innerHTML = result.join("");
+    populateStories(data);
     storyEventListeners();
     followEventListeners();
   } catch (e){
